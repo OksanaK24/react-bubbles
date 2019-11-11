90cd0054c1e187ce16f8d3a66270d555c925e299
Oksana Kril <oksanakril@Oksanas-iMac.local>
2019-11-10 18:36:10 -0600

    login page, privite route etc done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import api from "../utils/api";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [error, setError] = useState()
+	const [user, setUser] = useState({
+		username: "",
+		password: "",
+  })
+
+  const handleChange = (event) => {
+		setUser({
+			...user,
+			[event.target.name]: event.target.value,
+		})
+	}
+  
+  const handleSubmit = (event) => {
+		event.preventDefault()
+
+		api()
+			.post("/api/login", user)
+			.then(result => {
+				localStorage.setItem("token", result.data.payload)
+				props.history.push("/bubble_page")
+			})
+			.catch(err => {
+				setError(err.response.data.message)
+			})
+	}
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+
+      <form onSubmit={handleSubmit}>
+			{error && <div>{error}</div>}
+
+			<input type="username" name="username" placeholder="Username" value={user.username} onChange={handleChange} />
+			<input type="password" name="password" placeholder="Password" value={user.password} onChange={handleChange} />
+
+			<button type="submit">Login</button>
+		</form>
     </>
   );
 };
